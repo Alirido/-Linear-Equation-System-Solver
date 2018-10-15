e549839fd41344bc35f02fe31012b86cea588a23
Ali Rido <mohalirido@gmail.com>
2018-10-16 06:16:03 +0700

    Update Driver SPL

--- DriverSPL.java
+++ DriverSPL.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 class DriverSPL {
 	public static void main(String[] args) {
+
+		// Loading segment
+		System.out.println();
+		System.out.println();
+		System.out.print("\t");
+		for (int i=0; i<50; i++) {
+			System.out.print('~');
+
+			try {
+				Thread.sleep(100);
+			} catch(Exception e) {
+				System.out.println(e);
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.out.println();
+
 		Scanner in = new Scanner(System.in);
 		System.out.println("Choose input:");
 		System.out.println("1. Keyboard");
 		System.out.println("2. Text file");
-		System.out.print("What is your choice? ");
+		System.out.print("Enter your choice: ");
 		byte input=0;
 		while (input != 1 && input != 2) {
 			input = 0;
