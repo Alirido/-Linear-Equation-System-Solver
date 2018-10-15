f3fed8dafb815e70729e1b15ceaab2ee5e9f6e24
Ali Rido <mohalirido@gmail.com>
2018-10-16 03:50:04 +0700

    Update DriverSPL and add .gitignore

--- DriverSPL.java
+++ DriverSPL.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 class DriverSPL {
-	public class void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+		System.out.println("Choose input:");
+		System.out.println("1. Keyboard");
+		System.out.println("2. Text file");
+		byte input=0;
+		while (input != 1 && input != 2) {
+			input = 0;
+			try {
+				input = in.nextByte();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		if (input == 1) {
+			System.out.println("Input from keyboard:)");
+		} else System.out.println("Input from text file;)");
+
+		in.close();
 	}
 }
